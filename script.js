1ea60f380c7b86c5d95860b57c097e2e59c13514
GamySAF <gammiigamy@gmail.com>
2025-03-19 22:37:47 +0300

    Initial commit with landing page

---
+++ script.js
@@ -0,0 +1,10 @@
+// const dropdown = document.getElementById("drop-down");
+// const menuToggle = document.getElementById("showdropdowns");
+
+// function showdropdown() {
+//   dropdown.classList.add("show");
+// }
+
+// function hidedropdown() {
+//   dropdown.classList.remove("show");
+// }
